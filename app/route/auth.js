d3e63298ed02082b0434e205a0b411fcb00bad33
Bayu Prasetiyo <bayu.prasetiyo@paper.id>
2021-02-08 14:19:33 +0700

    fixing logout

--- app/route/auth.js
+++ app/route/auth.js
@@ -5,8 +5,7 @@ const AccessMidleware = require('../helpers/accessKey');
 
 
 router.post('/login', AuthController.login);
-router.get('/cek', AccessMidleware.checkAccess, AuthController.cek);
-router.get('/logout', AccessMidleware.checkAccess, AuthController.logout);
+router.get('/logout', AccessMidleware.checkAccess("all"), AuthController.logout);
 router.get('/ping', AuthController.ping);
 
 module.exports = router;
